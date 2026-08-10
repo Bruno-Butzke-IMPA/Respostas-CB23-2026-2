b592b62b7d05b59c4a0e2859b580ebc702e26094
Bruno <brunob@lastrocket.com.br>
2026-08-10 15:05:38 -0300

    feat: adição de modulo e programa em python

---
+++ Programa.py
@@ -0,0 +1,7 @@
+import MYTOOLS
+
+print('Digite o número para as constantes:')
+numero = int(input())
+
+print('Número de PI: '+ MYTOOLS.pi_real(numero))
+print('Número de Euler :'+ MYTOOLS.pi_real(numero))
