72108eeba85130752983758cdcf05d1e320b032a
Bruno-Butzke-IMPA <al.bruno.butzke@impatech.edu.br>
2026-09-15 11:22:01 -0300

    Delete Programa.py

--- Programa.py
+++
@@ -1,7 +0,0 @@
-import MYTOOLS
-
-print('Digite o número para as constantes:')
-numero = int(input())
-
-print('Número de PI: '+ MYTOOLS.pi_real(numero))
-print('Número de Euler :'+ MYTOOLS.pi_real(numero))
